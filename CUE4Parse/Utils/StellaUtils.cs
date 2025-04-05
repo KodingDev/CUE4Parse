efba32a0ab92a456364832308433710e3786b84b
Stella Inwood <kodingdev@pm.me>
2025-04-05 19:05:02 +1100

    fix: better version comparison

--- CUE4Parse/Utils/StellaUtils.cs
+++ CUE4Parse/Utils/StellaUtils.cs
@@ -1,29 +1,61 @@
-﻿namespace CUE4Parse.Utils;
+﻿using System;
+using System.Linq;
+
+namespace CUE4Parse.Utils;
 
 public class StellaUtils
 {
     public class MarvelRivalsVersion
     {
         private readonly string _version;
-        private readonly long _numericVersion;
+        private readonly int[] _components;
 
         public MarvelRivalsVersion(string version)
         {
             _version = version;
-            _numericVersion = long.Parse(version.Replace(".", ""));
+            var parts = version.Split('.');
+            _components = new int[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (!int.TryParse(parts[i], out _components[i]))
+                {
+                    throw new ArgumentException($"Version component '{parts[i]}' must be a valid integer", nameof(version));
+                }
+            }
         }
 
         public static implicit operator MarvelRivalsVersion(string version) => new(version);
 
-        public static bool operator >(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion > b._numericVersion;
-        public static bool operator <(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion < b._numericVersion;
-        public static bool operator >=(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion >= b._numericVersion;
-        public static bool operator <=(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion <= b._numericVersion;
-        public static bool operator ==(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion == b._numericVersion;
-        public static bool operator !=(MarvelRivalsVersion a, MarvelRivalsVersion b) => a._numericVersion != b._numericVersion;
+        public static bool operator >(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) > 0;
+        public static bool operator <(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) < 0;
+        public static bool operator >=(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) >= 0;
+        public static bool operator <=(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) <= 0;
+        public static bool operator ==(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) == 0;
+        public static bool operator !=(MarvelRivalsVersion a, MarvelRivalsVersion b) => Compare(a, b) != 0;
 
-        public override bool Equals(object? obj) => obj is MarvelRivalsVersion version && _numericVersion == version._numericVersion;
-        public override int GetHashCode() => _numericVersion.GetHashCode();
+        private static int Compare(MarvelRivalsVersion a, MarvelRivalsVersion b)
+        {
+            int maxLength = Math.Max(a._components.Length, b._components.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int aValue = i < a._components.Length ? a._components[i] : 0;
+                int bValue = i < b._components.Length ? b._components[i] : 0;
+
+                if (aValue != bValue)
+                    return aValue.CompareTo(bValue);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object? obj) =>
+            obj is MarvelRivalsVersion version && this == version;
+
+        public override int GetHashCode() =>
+            _components.Aggregate(17, (hash, component) => hash * 23 + component);
+
         public override string ToString() => _version;
     }
 
