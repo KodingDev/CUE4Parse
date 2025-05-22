208e293c172648fdc19476452f2847dbde937f5d
Stella Inwood <kodingdev@pm.me>
2025-05-22 20:17:31 +1000

    fix: string table with Marvel Rivals (actually)

--- CUE4Parse/UE4/Assets/Exports/Internationalization/FStringTable.cs
+++ CUE4Parse/UE4/Assets/Exports/Internationalization/FStringTable.cs
@@ -1,5 +1,6 @@
-﻿using CUE4Parse.UE4.Assets.Readers;
+﻿using System;
 using System.Collections.Generic;
+using CUE4Parse.UE4.Assets.Readers;
 using CUE4Parse.UE4.Objects.UObject;
 using CUE4Parse.UE4.Versions;
 
@@ -13,10 +14,23 @@ public class FStringTable
 
     public FStringTable(FAssetArchive Ar)
     {
+        // Copy the buffer
+        // var bytes = Ar.ReadBytes((int) (Ar.Length - Ar.Position));
+        // Console.WriteLine(Hex.ToHexString(bytes));
+
         TableNamespace = Ar.ReadFString();
 
-        KeysToEntries = Ar.ReadMap(Ar.ReadFString, Ar.ReadFString);
-        if (Ar.Game == EGame.GAME_Wildgate || Ar.Game == EGame.GAME_MarvelRivals) return;
+        KeysToEntries = Ar.ReadMap(Ar.ReadFString, () =>
+        {
+            var str = Ar.ReadFString();
+            if (Ar.Game == EGame.GAME_MarvelRivals)
+            {
+                // What does this even do?
+                Ar.Read<int>();
+            }
+            return str;
+        });
+        if (Ar.Game == EGame.GAME_Wildgate) return;
         KeysToMetaData = Ar.ReadMap(Ar.ReadFString, () => Ar.ReadMap(Ar.ReadFName, Ar.ReadFString));
     }
 }
